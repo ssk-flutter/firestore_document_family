2f95017a431886edc569d93047a3e26e89e4eb34
ssk <developer.kss@gmail.com>
2024-03-10 08:52:24 +0900

    test nothing

--- test/document_family_method_channel_test.dart
+++ test/document_family_method_channel_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:document_family/document_family_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelDocumentFamily platform = MethodChannelDocumentFamily();
   const MethodChannel channel = MethodChannel('document_family');
 
   setUp(() {
@@ -22,6 +20,5 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
   });
 }
--- test/document_family_test.dart
+++ test/document_family_test.dart
@@ -1,29 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:document_family/document_family.dart';
-import 'package:document_family/document_family_platform_interface.dart';
-import 'package:document_family/document_family_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockDocumentFamilyPlatform
-    with MockPlatformInterfaceMixin
-    implements DocumentFamilyPlatform {
-
+class MockDocumentFamilyPlatform with MockPlatformInterfaceMixin {
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 }
 
 void main() {
-  final DocumentFamilyPlatform initialPlatform = DocumentFamilyPlatform.instance;
 
-  test('$MethodChannelDocumentFamily is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelDocumentFamily>());
-  });
-
-  test('getPlatformVersion', () async {
-    DocumentFamily documentFamilyPlugin = DocumentFamily();
-    MockDocumentFamilyPlatform fakePlatform = MockDocumentFamilyPlatform();
-    DocumentFamilyPlatform.instance = fakePlatform;
-
-    expect(await documentFamilyPlugin.getPlatformVersion(), '42');
-  });
 }
