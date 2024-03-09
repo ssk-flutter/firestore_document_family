e045489a0d9380a41373a580f581cf0b28e411a8
ssk <developer.kss@gmail.com>
2024-03-10 08:43:47 +0900

    auto generated by runing example

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloud_firestore
+import document_family
+import firebase_core
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DocumentFamilyPlugin.register(with: registry.registrar(forPlugin: "DocumentFamilyPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+}
