6b78af0a58f90539e821871d6f9fe2fe087f7a44
ssk <developer.kss@gmail.com>
2024-03-10 08:49:16 +0900

    example 정리

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,28 +5,17 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
+          appBar: AppBar(
+            title: const Text('Plugin example app'),
+          ),
+          body: Center(
             child: ElevatedButton(
               onPressed: () async {
                 final DocumentFamily plugin = DocumentFamily.withPath(
